28daaa7dc410bd1f0617dff6c3edae4783c91dde
Eduardo <eduardoshibuka@gmail.com>
2018-03-17 00:09:14 +0000

    Added translation between bubbles

--- paper_onboarding/lib/UI/page_bubble.dart
+++ paper_onboarding/lib/UI/page_bubble.dart
@@ -9,30 +9,33 @@ class PageBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: new Container(
-          width: lerpDouble(25.0, 45.0, viewModel.activatePercent),
-          height: lerpDouble(25.0, 45.0, viewModel.activatePercent),
-          decoration: new BoxDecoration(
-            shape: BoxShape.circle,             
-            color: viewModel.isHollow
-              ? const Color(0X88FFFFFF).withAlpha((0X88 * viewModel.activatePercent).round())
-              : const Color(0X88FFFFFF),
-            border: new Border.all(
-              width: 3.0, 
+    return new Container(
+      width: 55.0,
+      height: 65.0,
+      child: new Center(
+        child: new Container(
+            width: lerpDouble(25.0, 45.0, viewModel.activatePercent),
+            height: lerpDouble(25.0, 45.0, viewModel.activatePercent),
+            decoration: new BoxDecoration(
+              shape: BoxShape.circle,             
               color: viewModel.isHollow
-              ? const Color(0X88FFFFFF).withAlpha((0X88 * (1.0 - viewModel.activatePercent)).round())
-              : Colors.transparent,
+                ? const Color(0X88FFFFFF).withAlpha((0X88 * viewModel.activatePercent).round())
+                : const Color(0X88FFFFFF),
+              border: new Border.all(
+                width: 3.0, 
+                color: viewModel.isHollow
+                ? const Color(0X88FFFFFF).withAlpha((0X88 * (1.0 - viewModel.activatePercent)).round())
+                : Colors.transparent,
+              ),
             ),
-          ),
-          child: new Opacity(
-            opacity: viewModel.activatePercent,
-            child: new Image.network(
-                viewModel.iconAssetPath,
-                color: viewModel.color,
-              )
-          )
+            child: new Opacity(
+              opacity: viewModel.activatePercent,
+              child: new Image.network(
+                  viewModel.iconAssetPath,
+                  color: viewModel.color,
+                )
+            )
+        ),
       ),
     );
   }
--- paper_onboarding/lib/UI/page_indicator.dart
+++ paper_onboarding/lib/UI/page_indicator.dart
@@ -11,55 +11,61 @@ class PageIndicator extends StatelessWidget {
   Widget build(BuildContext context) {
     List<PageBubble> bubbles = [];
 
-    for (var i = 0; i < viewModel.pages.length; i ++) {
+    for (var i = 0; i < viewModel.pages.length; i++) {
       final page = viewModel.pages[i];
-      
-      var percentActive;      
+
+      var percentActive;
       if (i == viewModel.activateIndex) {
         percentActive = 1.0 - viewModel.slidePercent;
-      } else if (i < viewModel.activateIndex
-          && viewModel.slideDirection == SlideDirection.leftToRight) {
+      } else if (i < viewModel.activateIndex &&
+          viewModel.slideDirection == SlideDirection.leftToRight) {
         percentActive = viewModel.slidePercent;
-
-      } else if (i > viewModel.activateIndex
-          && viewModel.slideDirection == SlideDirection.rightToLeft) {
+      } else if (i > viewModel.activateIndex &&
+          viewModel.slideDirection == SlideDirection.rightToLeft) {
         percentActive = viewModel.slidePercent;
-      }
-      else percentActive = 0.0;
+      } else
+        percentActive = 0.0;
 
-      bool isHollow = i > viewModel.activateIndex
-        || (i == viewModel.activateIndex
-              && viewModel.slideDirection == SlideDirection.leftToRight);
+      bool isHollow = i > viewModel.activateIndex ||
+          (i == viewModel.activateIndex &&
+              viewModel.slideDirection == SlideDirection.leftToRight);
 
-      bubbles.add(
-        new PageBubble(
-          viewModel: new PageBubbleViewModel(
-            page.iconAssetIcon,
-            page.color,
-            isHollow,
-            percentActive
-          ),
-        )
-      );
+      bubbles.add(new PageBubble(
+        viewModel: new PageBubbleViewModel(
+            page.iconAssetIcon, page.color, isHollow, percentActive),
+      ));
+    }
+
+    final BUBBLE_WIDTH = 55.0;
+    final baseTranslation = (viewModel.pages.length * BUBBLE_WIDTH / 2) - (BUBBLE_WIDTH / 2);
+    var translation = baseTranslation - (viewModel.activateIndex * BUBBLE_WIDTH);
+    switch (viewModel.slideDirection) {
+      case SlideDirection.leftToRight:
+        translation += BUBBLE_WIDTH * viewModel.slidePercent;
+        break;
+      case SlideDirection.rightToLeft:
+        translation -= BUBBLE_WIDTH * viewModel.slidePercent;
+        break;      
+      default:
+        break;
     }
 
     return new Column(
       children: <Widget>[
         new Expanded(child: new Container()),
-        new Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: bubbles,          
+        new Transform(
+          transform: new Matrix4.translationValues(translation, 0.0, 0.0),
+          child: new Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: bubbles,
+          ),
         )
       ],
     );
   }
 }
 
-enum SlideDirection { 
-  none, 
-  leftToRight, 
-  rightToLeft 
-}
+enum SlideDirection { none, leftToRight, rightToLeft }
 
 class PageIndicatorViewModel {
   final List<PageViewModel> pages;
